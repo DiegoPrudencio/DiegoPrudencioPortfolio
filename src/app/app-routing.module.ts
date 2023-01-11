9fa55100f788ef1de730d160c4ee18a2b508b98f
Diego <diegoprudencioo@gmail.com>
2023-01-11 20:17:48 -0300

    :sparkles: feat: main: add navbar, home and footer component (html and css)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TechstackComponent } from './pages/techstack/techstack.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' }, 
+  // {path: '/', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'techstack', component: TechstackComponent},
+  {path: 'projects', component: ProjectsComponent}, 
+  {path: 'contact', component: ContactComponent}, 
+  // {path: 'menu/caixaGeral/caixa', component: CaixaComponent}, 
+  // {path: 'menu/caixaGeral/caixa/cadastroDeTransacao', component: CadastroDeTransacaoComponent},
+  // {path: 'menu/caixaGeral/caixa/receita', component: ReceitaComponent}, 
+  // {path: 'menu/caixaGeral/caixa/despesa', component: DespesaComponent}, 
+  // {path: 'menu/configuracoes', component: ConfigComponent}
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TechstackComponent } from './pages/techstack/techstack.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './pages/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    TechstackComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+  NavbarComponent]
 })
 export class AppModule { }
