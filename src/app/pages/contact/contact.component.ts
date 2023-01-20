6c51003890a836f95981573e0227e9aa4929258c
Diego <diegoprudencioo@gmail.com>
2023-01-20 00:33:53 -0300

    :bug: fixt: main: bug corrections

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Validators } from '@angular/forms';
-
-import { Contact } from 'src/app/interface/contactForm';
-import { ContactService } from 'src/app/service/contact/contact.service';
 
 
 @Component({
@@ -13,71 +8,11 @@ import { ContactService } from 'src/app/service/contact/contact.service';
 })
 export class ContactComponent implements OnInit {
 
-  contactForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder
-    // , private contactService: ContactService
-    ) {
-
-    this.contactForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
-      phoneNumber: ['', [Validators.required, Validators.minLength(11)]],
-      email: ['', [Validators.required]],
-      subject: ['', [Validators.required]],
-      message: ['', [Validators.required]]
-    });
-  }
+  constructor() {}
 
   ngOnInit(): void {
   }
   
-  onSubmit(): void {
-    
-
-    const contact: Contact = ({
-      
-      name: this.contactForm.get('name')?.value,
-      phoneNumber: this.contactForm.get('phoneNumber')?.value,
-      email: this.contactForm.get('email')?.value,
-      subject: this.contactForm.get('subject')?.value,
-      message: this.contactForm.get('message')?.value,
-    })
-
-    alert("Still fixing this bug, please send a message on the phone or email!");
-
-    // this.contactService.sendEmail(contact).subscribe(res => {
-    //   if (res.ok) {
-    //     this.contactForm.reset();
-    //   } else {
-    //     console.error(res);
-    //   }
-    // });
-
-    // const email = this.contactForm.get('email').value;
-    // this.invalidEmail = this.regexp.test(email);
-    //Get the email element to apply some style to it.
-    // if(!this.invalidEmail){
-    //   this.emailDecorator(this.invalidEmail);
-    // }else{
-
-    //   //Remove change border color
-    //   this.emailDecorator(this.invalidEmail);
-
-    // const contact: Contact = ({
-    //   name: this.contactForm.get('name').value,
-    //   email: email,
-    //   message: this.contactForm.get('message').value
-    // })
-    //Service to handle the http POST request in the background
-    // this.contactService.sendEmail(contact).subscribe( res =>{
-    //   if(res.ok){
-    //     this.contactForm.reset();
-    //   }else{
-    //     console.error(res);
-    //   }
-    // });
-    // }
-
-  }
 
 }
